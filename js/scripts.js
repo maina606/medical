5f5b9d089962b92203f8e81bf92a9bca0e60e9ba
maina606 <cmmaina@gmail.com>
2019-03-06 16:28:59 +0300

    to push first aid page

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+  $("#clickable1").click(function(){
+    $("#showHide1").slideToggle();
+  });
+  $("#clickable2").click(function(){
+    $("#showHide2").slideToggle();
+  });
+
+$("#clickable3").click(function(){
+  $("#showHide3").slideToggle();
+});
+$("#clickable4").click(function(){
+  $("#showHide4").slideToggle();
+});
+$("#clickable5").click(function(){
+  $("#showHide5").slideToggle();
+});
+$("#clickable6").click(function(){
+  $("#showHide6").slideToggle();
+  });
+  $("#clickable7").click(function(){
+    $("#showHide7").slideToggle();
+    });
+    $("#clickable8").click(function(){
+      $("#showHide8").slideToggle();
+      });
+
+});
